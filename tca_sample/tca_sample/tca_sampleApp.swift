2a46e6f1ec1cf167b19ae9fc4cf5dcfa0d8c8d62
Takuya Sudo <someone.said.so@gmail.com>
2024-03-30 12:52:58 +0900

    kickoff

---
+++ tca_sample/tca_sample/tca_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_sampleApp.swift
+//  tca_sample
+//
+//  Created by takuyasudo on 2024/03/30.
+//
+
+import SwiftUI
+
+@main
+struct tca_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
